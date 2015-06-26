3317ed714ce808a12422e69d94666837fa05301e
Piotr Pasich <piotr.pasich@gmail.com>
2015-06-26 11:26:37 +0200

    Clean up

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,10 +33,5 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-
-        $envParameters = $this->getEnvParameters();
-        $loader->load(function($container) use($envParameters) {
-            $container->getParameterBag()->add($envParameters);
-        });
     }
 }
